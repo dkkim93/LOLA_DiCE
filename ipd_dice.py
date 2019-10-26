51687e4090ef4bd242c44f1bdd43f7baf9e23388
Dong-Ki Kim <dkkim93@mit.edu>
2019-10-25 23:24:33 -0400

    Changed: * Moved train related codes to trainer.py * Moved act function inside agent class

--- ipd_dice.py
+++ ipd_dice.py
@@ -1,13 +1,12 @@
 # coding: utf-8
-
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.distributions import Bernoulli
-from copy import deepcopy
-
 from envs import IPD
+from trainer import train
+
 
 class Hp():
     def __init__(self):
@@ -25,8 +24,10 @@ hp = Hp()
 
 ipd = IPD(hp.len_rollout, hp.batch_size)
 
+
 def magic_box(x):
     return torch.exp(x - x.detach())
+
 
 class Memory():
     def __init__(self):
@@ -73,32 +74,11 @@ class Memory():
         rewards = torch.stack(self.rewards, dim=1)
         return torch.mean((rewards - values)**2)
 
-def act(batch_states, theta, values):
-    batch_states = torch.from_numpy(batch_states).long()
-    probs = torch.sigmoid(theta)[batch_states]
-    m = Bernoulli(1-probs)
-    actions = m.sample()
-    log_probs_actions = m.log_prob(actions)
-    return actions.numpy().astype(int), log_probs_actions, values[batch_states]
-
 def get_gradient(objective, theta):
     # create differentiable gradient for 2nd orders:
     grad_objective = torch.autograd.grad(objective, (theta), create_graph=True)[0]
     return grad_objective
 
-def step(theta1, theta2, values1, values2):
-    # just to evaluate progress:
-    (s1, s2), _ = ipd.reset()
-    score1 = 0
-    score2 = 0
-    for t in range(hp.len_rollout):
-        a1, lp1, v1 = act(s1, theta1, values1)
-        a2, lp2, v2 = act(s2, theta2, values2)
-        (s1, s2), (r1, r2),_,_ = ipd.step((a1, a2))
-        # cumulate scores
-        score1 += np.mean(r1)/float(hp.len_rollout)
-        score2 += np.mean(r2)/float(hp.len_rollout)
-    return (score1, score2)
 
 class Agent():
     def __init__(self):
@@ -123,8 +103,8 @@ class Agent():
         (s1, s2), _ = ipd.reset()
         other_memory = Memory()
         for t in range(hp.len_rollout):
-            a1, lp1, v1 = act(s1, self.theta, self.values)
-            a2, lp2, v2 = act(s2, other_theta, other_values)
+            a1, lp1, v1 = self.act(s1, self.theta, self.values)
+            a2, lp2, v2 = self.act(s2, other_theta, other_values)
             (s1, s2), (r1, r2),_,_ = ipd.step((a1, a2))
             other_memory.add(lp2, lp1, v2, torch.from_numpy(r2).float())
 
@@ -136,8 +116,8 @@ class Agent():
         (s1, s2), _ = ipd.reset()
         memory = Memory()
         for t in range(hp.len_rollout):
-            a1, lp1, v1 = act(s1, self.theta, self.values)
-            a2, lp2, v2 = act(s2, other_theta, other_values)
+            a1, lp1, v1 = self.act(s1, self.theta, self.values)
+            a2, lp2, v2 = self.act(s2, other_theta, other_values)
             (s1, s2), (r1, r2),_,_ = ipd.step((a1, a2))
             memory.add(lp1, lp2, v1, torch.from_numpy(r1).float())
 
@@ -148,48 +128,21 @@ class Agent():
         v_loss = memory.value_loss()
         self.value_update(v_loss)
 
-def play(agent1, agent2, n_lookaheads):
-    joint_scores = []
-    print("start iterations with", n_lookaheads, "lookaheads:")
-    for update in range(hp.n_update):
-        # copy other's parameters:
-        theta1_ = torch.tensor(agent1.theta.detach(), requires_grad=True)
-        values1_ = torch.tensor(agent1.values.detach(), requires_grad=True)
-        theta2_ = torch.tensor(agent2.theta.detach(), requires_grad=True)
-        values2_ = torch.tensor(agent2.values.detach(), requires_grad=True)
+    def act(self, batch_states, theta, values):
+        batch_states = torch.from_numpy(batch_states).long()
+        probs = torch.sigmoid(theta)[batch_states]
+        m = Bernoulli(1-probs)
+        actions = m.sample()
+        log_probs_actions = m.log_prob(actions)
+        return actions.numpy().astype(int), log_probs_actions, values[batch_states]
 
-        for k in range(n_lookaheads):
-            # estimate other's gradients from in_lookahead:
-            grad2 = agent1.in_lookahead(theta2_, values2_)
-            grad1 = agent2.in_lookahead(theta1_, values1_)
-            # update other's theta
-            theta2_ = theta2_ - hp.lr_in * grad2
-            theta1_ = theta1_ - hp.lr_in * grad1
 
-        # update own parameters from out_lookahead:
-        agent1.out_lookahead(theta2_, values2_)
-        agent2.out_lookahead(theta1_, values1_)
-
-        # evaluate progress:
-        score = step(agent1.theta, agent2.theta, agent1.values, agent2.values)
-        joint_scores.append(0.5*(score[0] + score[1]))
-
-        # print
-        if update%10==0 :
-            p1 = [p.item() for p in torch.sigmoid(agent1.theta)]
-            p2 = [p.item() for p in torch.sigmoid(agent2.theta)]
-            print('update', update, 'score (%.3f,%.3f)' % (score[0], score[1]) , 'policy (agent1) = {%.3f, %.3f, %.3f, %.3f, %.3f}' % (p1[0], p1[1], p1[2], p1[3], p1[4]),' (agent2) = {%.3f, %.3f, %.3f, %.3f, %.3f}' % (p2[0], p2[1], p2[2], p2[3], p2[4]))
-
-    return joint_scores
-
-# plot progress:
 if __name__=="__main__":
-
     colors = ['b','c','m','r']
 
     for i in range(4):
         torch.manual_seed(hp.seed)
-        scores = play(Agent(), Agent(), i)
+        scores = train(Agent(), Agent(), i, hp, ipd)
         plt.plot(scores, colors[i], label=str(i)+" lookaheads")
 
     plt.legend()
---
+++ trainer.py
@@ -0,0 +1,56 @@
+import torch
+import numpy as np
+
+
+def evaluate(agent1, agent2, ipd, hp):
+    # just to evaluate progress:
+    (s1, s2), _ = ipd.reset()
+    score1 = 0
+    score2 = 0
+
+    theta1, values1 = agent1.theta, agent1.values
+    theta2, values2 = agent2.theta, agent2.values
+    for t in range(hp.len_rollout):
+        a1, lp1, v1 = agent1.act(s1, theta1, values1)
+        a2, lp2, v2 = agent2.act(s2, theta2, values2)
+        (s1, s2), (r1, r2), _, _ = ipd.step((a1, a2))
+        # cumulate scores
+        score1 += np.mean(r1) / float(hp.len_rollout)
+        score2 += np.mean(r2) / float(hp.len_rollout)
+    return (score1, score2)
+
+
+def train(agent1, agent2, n_lookaheads, hp, ipd):
+    joint_scores = []
+    print("start iterations with", n_lookaheads, "lookaheads:")
+
+    for update in range(hp.n_update):
+        # copy other's parameters:
+        theta1_ = torch.tensor(agent1.theta.detach(), requires_grad=True)
+        values1_ = torch.tensor(agent1.values.detach(), requires_grad=True)
+        theta2_ = torch.tensor(agent2.theta.detach(), requires_grad=True)
+        values2_ = torch.tensor(agent2.values.detach(), requires_grad=True)
+
+        for k in range(n_lookaheads):
+            # estimate other's gradients from in_lookahead:
+            grad2 = agent1.in_lookahead(theta2_, values2_)
+            grad1 = agent2.in_lookahead(theta1_, values1_)
+            # update other's theta
+            theta2_ = theta2_ - hp.lr_in * grad2
+            theta1_ = theta1_ - hp.lr_in * grad1
+
+        # update own parameters from out_lookahead:
+        agent1.out_lookahead(theta2_, values2_)
+        agent2.out_lookahead(theta1_, values1_)
+
+        # evaluate progress:
+        score = evaluate(agent1, agent2, ipd, hp)
+        joint_scores.append(0.5 * (score[0] + score[1]))
+
+        # print
+        if update % 10 == 0:
+            p1 = [p.item() for p in torch.sigmoid(agent1.theta)]
+            p2 = [p.item() for p in torch.sigmoid(agent2.theta)]
+            print('update', update, 'score (%.3f,%.3f)' % (score[0], score[1]) , 'policy (agent1) = {%.3f, %.3f, %.3f, %.3f, %.3f}' % (p1[0], p1[1], p1[2], p1[3], p1[4]),' (agent2) = {%.3f, %.3f, %.3f, %.3f, %.3f}' % (p2[0], p2[1], p2[2], p2[3], p2[4]))
+
+    return joint_scores
